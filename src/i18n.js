8fc750154e8ed56aaf22c8790123e2923be1a209
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-01-30 11:57:30 +0100

    Added multilanguage configuration

---
+++ src/i18n.js
@@ -0,0 +1,33 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import translationEN from "./locales/en/translation.json";
+import translationPL from "./locales/pl/translation.json";
+
+const resources = {
+	en: {
+		translation: translationEN,
+	},
+	pl: {
+		translation: translationPL,
+	},
+};
+
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		resources,
+		fallbackLng: "pl",
+		interpolation: {
+			escapeValue: false,
+		},
+		detection: {
+			order: ["localStorage", "navigator"],
+			caches: ["localStorage"],
+			lookupLocalStorage: "i18nextLng",
+		},
+	});
+
+export default i18n;
